515ef9f48a66216b0ec4dd5ff5bf48c49c810c1e
Filipe Costa <116469491+c0sta90@users.noreply.github.com>
2024-01-03 12:28:27 -0100

    Add files via upload

--- euro.cpp
+++ euro.cpp
@@ -1,42 +1,21 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
+using namespace std;
 
-void key_euro(int num[], int star[]) {
-    int i;
-    srand(time(NULL)); 
+int MyArray[] {1, 2, 3, 4, 5 };
 
-    for (i = 0; i < 5; i++) {
-        num[i] = rand() % 50 + 1;
-    }
+string letras [2] [4];
 
-    for (i = 0; i < 2; i++) {
-        star[i] = rand() % 12 + 1;
+string letras2 [2] [4] {
+              {"A", "B", "C", "D"},
+              {"E", "F", "G", "H"},    
+};
+
+int main(){
+
+    for (int i = 0; i < 2; i++){
+     for (int j = 0; j < 4; j++){
+
+        cout << letras2[i][j] << endl;
     }
 }
-
-int main() {
-    int num[5];
-    int star[2];
-    int i;
-    char response;
-
-    do {
-        key_euro(num, star);
-
-        std::cout << "Números: ";
-        for (i = 0; i < 5; i++) {
-            std::cout << num[i] << " ";
-        }
-
-        std::cout << "\nEstrela: ";
-        for (i = 0; i < 2; i++) {
-            std::cout << star[i] << " ";
-        }
-
-        std::cout << "\nDeseja uma nova chave? (S/N): ";
-        std::cin >> response;
-    } while (response == 'S' || response == 's');
-
-    return 0;
 }
